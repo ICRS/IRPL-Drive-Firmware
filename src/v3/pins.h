ff6e72bd65003cd8e797574561676685b2fc4aff
Ben <bens15351@gmail.com>
2025-08-26 19:02:57 +0200

    Pin fix attempt 1

--- src/v3/pins.h
+++ src/v3/pins.h
@@ -1,14 +1,27 @@
-#define EN_L 19
-#define PWM_L 18
+// #define EN_L 19
+// #define PWM_L 18
+// #define SDA 21
+// #define SCL 22
+// #define IN1_L 23
+// #define IN2_L 25
+// #define EN_R 16
+// #define IN1_R 17
+// #define IN2_R 5
+// #define PWM_R 4
+// #define SCK 
+// #define MISO
+// #define MOSI
+// #define SS
+
+#define PWM_L EN_L
+#define EN_L 18
+#define IN1_L 19
+#define IN2_L 23
+
+#define PWM_R EN_R
+#define EN_R 4
+#define IN1_R 16
+#define IN2_R 17
+
 #define SDA 21
 #define SCL 22
-#define IN1_L 23
-#define IN2_L 25
-#define EN_R 16
-#define IN1_R 17
-#define IN2_R 5
-#define PWM_R 4
-#define SCK 
-#define MISO
-#define MOSI
-#define SS
